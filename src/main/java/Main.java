f5fde085637a8bf1c64f834176f462fe1e66c559
tomsandalon <48441589+tomsandalon@users.noreply.github.com>
2022-01-07 16:08:37 +0200

    handoff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,6 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
-import org.apache.hadoop.mapreduce.TestMapCollection.StepFactory;
 
 
 
@@ -19,11 +18,11 @@ public class Main {
     private static final String S3_BUCKET_NAME = "distributed-systems-assignment-2";
     private static final String S3_BUCKET_URL = "s3n://" + S3_BUCKET_NAME + "/";
     private static final String OUTPUT_FOLDER_NAME = "output";
-    private static final String OUTPUT = S3_BUCKET_URL + OUTPUT_FOLDER_NAME;
+    private static final String FINAL_OUTPUT_FOLDER = S3_BUCKET_URL + OUTPUT_FOLDER_NAME;
     private static final String LOGS = S3_BUCKET_URL + "logs";
     private static final String _3_GRAM_DATASET = "s3n://datasets.elasticmapreduce/ngrams/books/20090715/heb-all/3gram/data";
+//    private static final String _3_GRAM_DATASET = "s3n://distributed-systems-assignment-2/3_grams.txt";
     private static final String READ_ONLY_FILE_EXTENSION = "-r-00000";
-    StepFactory stepFactory = new StepFactory();
 
     private static String jarOfStep(int stepNum) {
         return String.format("s3n://%s/step%s.jar", S3_BUCKET_NAME, stepNum);
@@ -72,7 +71,7 @@ hadoop jar /Users/michaelamar/Desktop/Hadoop-Word-Prediction/out/artifacts/Step6
                 hadoopJarStepConfig = hadoopJarStepConfig.withArgs(outputFolderName(3), outputFolderName(4), outputFolderForStep(step));
                 break;
             case 6:
-                hadoopJarStepConfig = hadoopJarStepConfig.withArgs(outputFolderName(5), outputFolderForStep(step));
+                hadoopJarStepConfig = hadoopJarStepConfig.withArgs(outputFolderName(5), FINAL_OUTPUT_FOLDER);
                 break;
         }
 
